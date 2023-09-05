0b55236ce8240d8c09551f03de50d4d53a8979fa
kiatklai <likhitthaworn.k@gmail.com>
2023-09-05 20:53:15 +0900

    component作成

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import HeaderCart from "./HeaderCart"
+
+const Header=()=>{
+  return(
+    <header className="header">
+      <h1>Ooh-Gotto.com</h1>
+      <HeaderCart/>
+    </header>
+  )
+}
+
+export default Header
---
+++ src/components/HeaderCart.js
@@ -0,0 +1,10 @@
+const HeaderCart=()=>{
+  return(
+    <button className="button">
+      <span>カート</span>
+      <span className="badge">10</span>
+    </button>
+  )
+}
+
+export default HeaderCart
