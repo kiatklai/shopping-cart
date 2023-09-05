c5a3952849c7c039f9e9f651f8dc14db82155d75
kiatklai <likhitthaworn.k@gmail.com>
2023-09-05 22:02:57 +0900

    内容のデザイン変更

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -6,7 +6,7 @@ const Cart=()=>{
     <div className="shopping-cart">
       <div className="title">カート内の商品</div>
       {CartData.map((data)=>{
-        return <CartItem key={data.id}/>
+        return <CartItem key={data.id} {...data}/>
       })}
       <div className="footer">合計 5 円</div>
     </div>
--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,7 +1,13 @@
-const CartItem=()=>{
+const CartItem=({id,name,image_url,price,quantity})=>{
   return(
-    <div>
-      <h2>Cart Item</h2>
+    <div className="item">
+      <div className="product_image">
+        <img src={image_url} alt=""/>
+      </div>
+      <div className="description">
+        <span>{name}</span>
+        <span>値段 {price} 円</span>
+      </div>
     </div>
   )
 }
