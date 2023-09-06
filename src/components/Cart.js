a3dbbca5e7ae44484273563da36b5b8c7da904bd
kiatklai <likhitthaworn.k@gmail.com>
2023-09-06 21:59:02 +0900

    context/reducer

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,11 +1,13 @@
-import CartData from '../data/CartData'
+
+import { MyCartContext } from '../management/context'
 import CartItem from './CartItem'
 
 const Cart=()=>{
+  const {cart} = MyCartContext()
   return(
     <div className="shopping-cart">
       <div className="title">カート内の商品</div>
-      {CartData.map((data)=>{
+      {cart.map((data)=>{
         return <CartItem key={data.id} {...data}/>
       })}
       <div className="footer">合計 5 円</div>
---
+++ src/management/context.js
@@ -0,0 +1,25 @@
+import {createContext,useReducer,useContext} from "react"
+import CartData from "../data/CartData"
+import reducer from "./reducer"
+
+const initState={
+  cart:CartData,
+  total:0,
+  amount:0
+}
+const CartContext = createContext()
+
+export const MyCartContext=()=>{
+  return useContext(CartContext)
+}
+
+const CartProvider=({children})=>{
+  const [state,dispatch] = useReducer(reducer,initState)
+  return(
+    <CartContext.Provider value={{...state}}>
+      {children}
+    </CartContext.Provider>
+  )
+}
+
+export {CartContext,CartProvider}
